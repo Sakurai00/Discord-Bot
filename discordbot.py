139b4a8a5d00df5d0548617f87d65572552c2ee7
Sakurai <K.Sakurai_2000@outlook.com>
2020-12-23 19:27:48 +0900

    add #3

--- discordbot.py
+++ discordbot.py
@@ -1,12 +1,13 @@
 import discord
 from discord.ext import commands
 import settings as st
+import random
 
 
 discord_intents = discord.Intents.all()
 
-bot = commands.Bot(
-    command_prefix = "!",
+client = commands.Bot(
+    command_prefix = "$",
     case_insensitive=True,
     intents = discord_intents,
     activity = discord.Game("Bot test")
@@ -14,22 +15,37 @@ bot = commands.Bot(
 
 
 
-@bot.event
+@client.event
 async def on_ready():
-    print(discord.__version__ + "ready")
+    print("v" + discord.__version__ + " ready")
 
-@bot.command()
+
+@client.command()
 async def whoami(ctx):
     await ctx.send(ctx.message.author.name)
 
 
+@client.command()
+async def roll(ctx, dice : str):
+    try:
+        rolls, limit = map(int, dice.split("d"))
+    except Exception:
+        return
+    ans = ""
+    sum = 0
+    for r in range(rolls):
+        tmp = random.randint(1, limit)
+        sum += tmp
+        ans += (str(tmp) + " ")
+    result = ans + " " + str(sum)
+    await ctx.send(result)
 
 
 def main():
     try:
-        bot.loop.run_until_complete(bot.start(st.TOKEN))
+        client.loop.run_until_complete(client.start(st.TOKEN))
     except KeyboardInterrupt:
-        bot.loop.run_until_complete(bot.close())
+        client.loop.run_until_complete(client.close())
 
 
 if __name__ == '__main__':
